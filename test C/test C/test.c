5541924493bec4095bfd5db23638fcff901f87c8
杨丹 <2901439120@qq.com>
2022-12-17 22:00:00 +0800

    基础练习

---
+++ test C/test C/test.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+
+//int b = 1000;
+//int main()
+//{
+//	int a = 100;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int a = 1000;
+//int main()
+//{
+//	int a = 100;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	/*const int num = 10;
+//	printf("%d\n", num);
+//	num = 20;
+//	printf("%d\n", num);*/
+//
+//	int arr1[10] = { 0 };
+//	const int n = 10;
+//	int arr2[n] = { 0 };
+//	return 0;
+//}
+
+//#define M 100 //M是一个常量,将100赋值给M
+//int main()
+//{
+//	int a = M;
+//	int arr[M] = { 0 };
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	enum Color
+//	{
+//		RED,
+//		GREEN,
+//		BLUE
+//	};
+//	printf("%d %d %d ", RED, GREEN, BLUE);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abc"; 
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	/*printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	printf("%s\n", arr3);*/
+//	return 0;
+//}
+
+int main()
+{
+	printf("%c\n",'\130');
+	printf("%c\n", '\x31');
+
+	return 0;
+}
